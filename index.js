a7f226fd5fdd748db5b1c053bfc9a74c84b8acdf
Muhammad Mouiz Ghouri <mouizghouri77@gmail.com>
2023-09-11 13:35:54 +0500

    fixes

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
+const port = process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 
 let appData = {
@@ -140,6 +141,6 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+server.listen(port, '0.0.0.0', () => {
+    console.log(`listening on *:${port}`);
 });
